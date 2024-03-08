ae4ff9a5783674701fe0b583b4e3641da91e8db9
mx-pai <146422771+mx-pai@users.noreply.github.com>
2024-03-08 20:27:48 +0800

    更改

--- miniprogram/pages/custom_cm/custom_cm.js
+++ miniprogram/pages/custom_cm/custom_cm.js
@@ -1,34 +1,12 @@
 Page({
   data: {
-    functions: [
-      {
-        "name": "全部订单",
-        "pic_url": '/images/iron/dingdan.png'
-      },
-      {
-        "name": "待支付",
-        "pic_url": '../../images/iron/zhifu.png'
-      },
-      {
-        "name": "待发货",
-        "pic_url": '../../images/iron/fahuo.png'
-      },
-      {
-        "name": "待收货",
-        "pic_url": '../../images/iron/shouhuo.png'
-      },
-      {
-        "name": "地址信息",
-        "pic_url": '../../images/iron/dizhi.png'
-      },
-      {
-        "name": "待评价",
-        "pic_url": '../../images/iron/pingjia.png'
-      },
-      {
-        "name": "消息",
-        "pic_url": '../../images/iron/xiaoxi.png'
-      }
-    ]
+    
+  },
+  gotoPage(){
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
   }
+  
+  
 })
