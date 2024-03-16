a1b781b6e3dc55dbe986519bce99680146d8bd58
kingpoem <149233011+kingpoem@users.noreply.github.com>
2024-03-16 13:27:58 +0800

    upload custom_cm
    优化页面样式

--- miniprogram/pages/custom_cm/custom_cm.js
+++ miniprogram/pages/custom_cm/custom_cm.js
@@ -1,57 +1,56 @@
 Page({
-    data: {
+  data: {
 
-    },
-    gotoPage() {
-        wx.navigateTo({
-            url: '/pages/login/login',
-        })
-    },
-    gotoall_orders() {
-        wx.navigateTo({
-            url: '/pages/all_orders/all_orders?currentIndex=0',
-        })
-    },
-    gotopending_payment() {
-        wx.navigateTo({
-            url: '/pages/all_orders/all_orders?currentIndex=1',
-        })
-    },
-    gotopending_shipment() {
-        wx.navigateTo({
-            url: '/pages/all_orders/all_orders?currentIndex=2',
-        })
-    },
-    gotopending_delivery() {
-        wx.navigateTo({
-            url: '/pages/all_orders/all_orders?currentIndex=3',
-        })
-    },
-    gotopending_evaluation() {
-        wx.navigateTo({
-            url: '/pages/all_orders/all_orders?currentIndex=4',
-        })
-    },
-    gotoaddress_management() {
-        wx.navigateTo({
-                url: '/packageB/pages/address_management/address_management',
-            }),
-            console.log(1);
-    },
-    gotomessage() {
-        wx.navigateTo({
-            url: '/pages/message/message',
-        })
-    },
-    gotointelligent_assistant() {
-        wx.navigateTo({
-            url: '/pages/intelligent_assistant/intelligent_assistant',
-        })
+  },
+  gotoPage() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
+  },
+  gotoall_orders() {
+    wx.navigateTo({
+      url: '/pages/all_orders/all_orders?currentIndex=0',
+    })
+  },
+  gotopending_payment() {
+    wx.navigateTo({
+      url: '/pages/all_orders/all_orders?currentIndex=1',
+    })
+  },
+  gotopending_shipment() {
+    wx.navigateTo({
+      url: '/pages/all_orders/all_orders?currentIndex=2',
+    })
+  },
+  gotopending_delivery() {
+    wx.navigateTo({
+      url: '/pages/all_orders/all_orders?currentIndex=3',
+    })
+  },
+  gotopending_evaluation() {
+    wx.navigateTo({
+      url: '/pages/all_orders/all_orders?currentIndex=4',
+    })
+  },
+  gotoaddress_management() {
+    wx.navigateTo({
+      url: '/packageB/pages/address_management/address_management'
+    })
+  },
+  gotomessage() {
+    wx.navigateTo({
+      url: '/pages/message/message',
+    })
+  },
+  gotointelligent_assistant() {
+    wx.navigateTo({
+      url: '/pages/intelligent_assistant/intelligent_assistant',
+    })
 
-    },
-    onLoad() {
-        getApp().editTabbar();
-    }
+  },
+  onLoad() {
+    getApp().editTabbar();
+  }
 
 
 })
